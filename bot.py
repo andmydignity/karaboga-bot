a3496566a803a9a26b9d24470109b81d6a3fe73b
Semih Aslan <ahmetaslan139@outlook.com.tr>
2024-08-28 20:16:35 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,26 @@
 import praw
-from time import sleep
+
+reddit = praw.Reddit(#Creds go here)
+
 wordl = ["tayyip", "erdoğan", "teyyip", "turkey", "rte", "r.t.e",
          "r.t.e.", "uzun adam", "turgay", "nigga", "karaboga", "karaboğa", "türkiye", "turk", "türk", "cockroach",
-         "erdogan", "arab", "armenian genocide", "atatürk", "ataturk", "atagay", "erdogay", "turkish", "t"]
-
-reddit = praw.Reddit()#creditansials here
+         "erdogan", "arab", "armenian genocide", "atatürk", "ataturk", "atagay", "erdogay", "turkish", "turks"]
 
 
-subl = ["balkans_irl"]  # Subs that bot will reply on
-d = 0
-
-# Move the set initialization outside the loop
-last=""
-wait=3
-is_k=False
-while True:
-    sub = reddit.subreddit(subl[d])
-    print(subl[d])
-
-    for post in sub.new(limit=1):
-        if post.id==last:
-            print("skipped")
-            continue
-        post_title_words = post.title.lower().split()  # Split the title into words
+text = "The brand new Dutchfucker-3000 (developed proudly in Republic of Turkiye) is here to inform you about: \n\n https://discord.gg/5vDpxDrb9f - For even more brainrot.\n\n https://balkansirl.net \n\n Stay tuned."
+text_k="The brand new Dutchfucker-3000 (developed proudly in Republic of Turkiye) is here to inform you about: \n\n https://discord.gg/5vDpxDrb9f - For even more brainrot.\n\n https://balkansirl.net \n\n Stay tuned. \n\n Also, \n\n\n\n K"
+for submission in reddit.subreddit("balkans_irl").stream.submissions(skip_existing=True):
+     try:
+        is_k=False
+        post_title_words = submission.title.lower().split()  # Split the title into words
         for word in post_title_words:
             if word in wordl:
-                print(f"Found a relevant word in post: {post.title.lower()}")
                 is_k=True
-                break  # Exit the inner loop if a relevant word is found
-        try:
-            if is_k:
-                rep_temp="hi favorite (i hope so) balkan bot here, here's some links for some reason \n\n [imageboard](http://balkansirl.net) [discord](https://discord.gg/balkansirl) \n\n also you can click [here](https://www.reddit.com/message/compose/?to=/r/balkans_irl) to contact the mods. \n\n Also, \n\n\n\n K"
+                break
             else:
-                rep_temp="hi favorite (i hope so) balkan bot here, here's some links for some reason \n\n [imageboard](http://balkansirl.net) [discord](https://discord.gg/balkansirl) \n\n also you can click [here](https://www.reddit.com/message/compose/?to=/r/balkans_irl) to contact the mods."
-            post.reply(rep_temp)
-            print("Replied to a post.(" + subl[d] + ")")
-        except Exception as e:
-            print("Couldn't reply. ({})".format(e))
-        last=post.id
-        is_k=False
-
-    if d == len(subl) - 1:
-        d = 0
-    else:
-        d += 1
-    print("waiting")
-    sleep(wait)
-    # Switch to another sub
+                is_k=False
+        if is_k:
+            submission.reply(text_k)
+        else:
+            submission.reply(text)
+     except:
